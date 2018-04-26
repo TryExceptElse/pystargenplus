2133c0a1c8a2b79f38a96bb4dc46b3fec1d5a83f
tryexceptelse <schwarzjohnv@gmail.com>
2018-04-25 19:38:42 -0700

    Tests now use consistent seeds for System

--- test/test_stargen.py
+++ test/test_stargen.py
@@ -6,16 +6,20 @@ from sgp.stargen import System, SunConfig, InvalidStateException, \
     SunView, PlanetView
 
 
+# Note: For all system generation tests, set a seed value to ensure
+# tests are repeatable. Otherwise a random seed will be used.
+
+
 class TestSystemGeneration(TestCase):
     def test_system_generation_will_not_generate_twice(self):
         sun = SunConfig(1, 1)
-        system = System(sun_config=sun)
+        system = System(rng_seed=24, sun_config=sun)
         system.generate()
         self.assertRaises(InvalidStateException, system.generate)
 
     def test_system_is_marked_as_generated_after_generation(self):
         sun = SunConfig(1, 1)
-        system = System(sun_config=sun)
+        system = System(rng_seed=24, sun_config=sun)
         system.generate()
         self.assertTrue(system.generated)
 
@@ -23,7 +27,7 @@ class TestSystemGeneration(TestCase):
         sun_mass = 0.43
         sun_luminosity = 0.31
         sun_config = SunConfig(sun_mass, sun_luminosity)
-        system = System(sun_config=sun_config)
+        system = System(rng_seed=24, sun_config=sun_config)
         system.generate()
         sun = system.sun
         self.assertIsNotNone(sun)
@@ -31,7 +35,7 @@ class TestSystemGeneration(TestCase):
         self.assertEqual(sun.luminosity, sun_luminosity)
 
     def test_system_default_sun_has_valid_data(self):
-        system = System()
+        system = System(rng_seed=24)
         system.generate()
         sun = system.sun
         self.assertIsNotNone(sun)
@@ -39,13 +43,13 @@ class TestSystemGeneration(TestCase):
         self.assertGreater(sun.luminosity, 0)
 
     def test_generated_system_has_planets(self):
-        system = System()
+        system = System(rng_seed=24)
         system.generate()
         planets = [planet for planet in system.planets]
         self.assertGreater(len(planets), 0)
 
     def test_moons_are_generated(self):
-        system = System(do_moons=True)
+        system = System(rng_seed=24, do_moons=True)
         system.generate()
         moons = [moon for moon in itr.chain(
             [[moon for moon in planet.moons] for planet in system.planets])]
@@ -61,6 +65,17 @@ class TestSystemGeneration(TestCase):
         self.assertEqual(planets_a[0].mass, planets_b[0].mass)
         self.assertEqual(planets_a[1].mass, planets_b[1].mass)
         self.assertEqual(planets_a[2].mass, planets_b[2].mass)
+
+    def test_systems_generated_with_different_seeds_are_not_identical(self):
+        system_a = System(rng_seed=24, do_moons=True)
+        system_a.generate()
+        system_b = System(rng_seed=25, do_moons=True)
+        system_b.generate()
+        planets_a = [i for i in system_a.planets]
+        planets_b = [i for i in system_b.planets]
+        self.assertNotEqual(planets_a[0].mass, planets_b[0].mass)
+        self.assertNotEqual(planets_a[1].mass, planets_b[1].mass)
+        self.assertNotEqual(planets_a[2].mass, planets_b[2].mass)
 
 
 class TestSunConfig(TestCase):
